793aca470f40a3fece163dd3faacb8fdd260a9e6
Biagio Distefano <biagiodistefano92@gmail.com>
2019-04-01 18:10:19 +0200

    added flags tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,18 @@
 import unittest
 import os
+import re
 
 from replus import Engine
 
 __HERE__ = os.path.dirname(os.path.abspath(__file__))
 
 test_models_path = os.path.join(__HERE__, "test_models")
-engine = Engine(test_models_path)
 
 
 class TestEngine(unittest.TestCase):
 
     def test_parser_regex(self):
+        engine = Engine(test_models_path)
         patterns = [p.pattern for k, p, t in engine.patterns if k == "tests"]
         expected = [
             r"This is an unnamed number group: (?:\d).",
@@ -20,7 +21,16 @@ class TestEngine(unittest.TestCase):
         for i, p in enumerate(patterns):
             self.assertEqual(p, expected[i])
 
+    def test_flags(self):
+        engine_i = Engine(test_models_path, *"i")
+        matches = engine_i.parse("Today it's January 1st 1970")
+        self.assertEqual(len(matches), 1)
+        engine = Engine(test_models_path)
+        matches = engine.parse("Today it's January 1st 1970")
+        self.assertEqual(len(matches), 0)
+
     def test_match(self):
+        engine = Engine(test_models_path)
         matches = engine.parse("Today it's january 1st 1970")
         self.assertEqual(len(matches), 1)
         date = matches[0]
