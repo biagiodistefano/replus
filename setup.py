a5e3a0342c581b27c10f5038bfe9e753a88039ba
Biagio Distefano <biagiodistefano92@gmail.com>
2022-04-07 00:38:11 +0200

    minor refactor

--- setup.py
+++ setup.py
@@ -20,16 +20,16 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-def read(*parts):
+def read(filepath):
     """Taken from pypa pip setup.py:
     intentionally *not* adding an encoding option to open, See:
        https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
     """
-    return codecs.open(os.path.join(here, *parts), 'r').read()
+    return codecs.open(here / filepath, 'r').read()
 
 
-def find_version(*file_paths):
-    version_file = read(*file_paths)
+def find_version(init_path):
+    version_file = read(init_path)
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
                               version_file, re.M)
     if version_match:
@@ -39,7 +39,7 @@ def find_version(*file_paths):
 
 setup(
     name='replus',
-    version=find_version("replus", "__init__.py"),
+    version=find_version("replus/__init__.py"),
     description="A wrapper for the regex library for advanced regex pattern management",
     long_description=read('README.rst'),
     classifiers=[
